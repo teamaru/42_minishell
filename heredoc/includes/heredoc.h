1995b7a899fc22e6bbbb43e6ea362bb999846e20
public-jun <generalpublicjunjun@gmail.com>
2021-10-06 23:27:04 +0900

    Add: heredocumentの作業dirを作成
    About: #9

---
+++ heredoc/includes/heredoc.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   heredoc.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/10/06 23:21:49 by jnakahod          #+#    #+#             */
+/*   Updated: 2021/10/06 23:23:42 by jnakahod         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEREDOC_H
+# define HEREDOC_H
+
+#include <stdio.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+#include "../../libft/includes/libft.h"
+
+#endif

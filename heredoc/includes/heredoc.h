13256cb308d2b553a70ec11e806c872d190dd79c
public-jun <generalpublicjunjun@gmail.com>
2021-10-07 16:48:59 +0900

    Add: 親プロセスでreadlineで受けた文字列でheredocを作成
    Task:
        - pipe()を呼び出してHere Document用のパイプを生成
        - write(herepipe[1],...)で書き込みを行う
    
        -
        parser部分で'や"で囲まれている文字はそのままtokenに格納して、t_pipe_list作成時に展開するかの判断
        - 閉じられていないクォートの扱い方
    
    About: #9

--- heredoc/includes/heredoc.h
+++ heredoc/includes/heredoc.h
@@ -6,7 +6,7 @@
 /*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 23:21:49 by jnakahod          #+#    #+#             */
-/*   Updated: 2021/10/06 23:23:42 by jnakahod         ###   ########.fr       */
+/*   Updated: 2021/10/07 16:18:49 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,5 +17,11 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 #include "../../libft/includes/libft.h"
+#include <unistd.h>
+#include <string.h>
+#include <stdio.h>
+#include <errno.h>
+#include <signal.h>
+#include <fcntl.h>
 
 #endif
--- heredoc/srcs/main.c
+++ heredoc/srcs/main.c
@@ -6,20 +6,69 @@
 /*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/06 23:16:19 by jnakahod          #+#    #+#             */
-/*   Updated: 2021/10/06 23:24:21 by jnakahod         ###   ########.fr       */
+/*   Updated: 2021/10/07 16:48:42 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <heredoc.h>
 
 /*
-** heredocのdemo作成
+** 親プロセスでheredocment生成
 **
 **
 */
 
-int	main(void)
+void	free_set(void **dst, void *src)
 {
-	printf("Hello heredoc!\n");
+	free(*dst);
+	*dst = src;
+}
+
+char	*update_heredoc(char **old, char *input)
+{
+	char *old_end_newline;
+	char *new_heredoc;
+
+	if (!*old)
+		return (ft_strdup(input));
+	else
+	{
+		old_end_newline = NULL;
+		new_heredoc = NULL;
+		old_end_newline = ft_strjoin(*old, "\n");
+		free_set((void **)old, NULL);
+		if (!old_end_newline)
+			return (NULL);
+		new_heredoc = ft_strjoin(old_end_newline, input);
+		free_set((void **)&old_end_newline, NULL);
+		if (!new_heredoc)
+			return (NULL);
+	}
+	return (new_heredoc);
+}
+
+int	main(int ac, char **av)
+{
+	char	*input;
+	char	*heredoc;
+
+	if (ac != 2)
+		return (-1);
+	heredoc = NULL;
+	while (1)
+	{
+		input = readline("> ");
+		if (!input || !ft_strncmp(input, av[1], ft_strlen(av[1]) + 1))
+		{
+			free_set((void **)&input, NULL);
+			break;
+		}
+		heredoc = update_heredoc(&heredoc, input);
+		add_history(input);
+		free_set((void **)&input, NULL);
+	}
+	printf("%s", heredoc);
+	rl_clear_history();
+	free_set((void **)&heredoc, NULL);
 	return (0);
 }

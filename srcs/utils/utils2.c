a824c52eed707bdea9222ccb540775d84faae8c9
public-jun <generalpublicjunjun@gmail.com>
2021-10-19 23:10:56 +0900

    Add: norm #36

--- srcs/utils/utils2.c
+++ srcs/utils/utils2.c
@@ -3,29 +3,29 @@
 /*                                                        :::      ::::::::   */
 /*   utils2.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsugiyam <tsugiyam@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/06 21:36:24 by tsugiyam          #+#    #+#             */
-/*   Updated: 2021/06/06 21:36:24 by tsugiyam         ###   ########.fr       */
+/*   Updated: 2021/10/19 22:52:42 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mini_shell.h>
 
-t_bool is_chunk(char c)
+t_bool	is_chunk(char c)
 {
-  return (c && !is_white(c));
+	return (c && !is_white(c));
 }
 
-t_bool is_quote(char c)
+t_bool	is_quote(char c)
 {
-  return (c == SGL_QT || c == DBL_QT);
+	return (c == SGL_QT || c == DBL_QT);
 }
 
-t_bool is_end(char *line)
+t_bool	is_end(char *line)
 {
-  clear_white(&line);
-  if (!*line)
-    return (TRUE);
-  return (FALSE);
+	clear_white(&line);
+	if (!*line)
+		return (TRUE);
+	return (FALSE);
 }
--- srcs/utils/utils3.c
+++ srcs/utils/utils3.c
@@ -6,13 +6,13 @@
 /*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/16 12:25:40 by jnakahod          #+#    #+#             */
-/*   Updated: 2021/10/18 20:04:30 by jnakahod         ###   ########.fr       */
+/*   Updated: 2021/10/19 22:53:05 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mini_shell.h>
 
-t_bool is_match_str(char *input, char *delimiter)
+t_bool	is_match_str(char *input, char *delimiter)
 {
 	if (!ft_strncmp(input, delimiter, ft_strlen(delimiter) + 1))
 		return (TRUE);
@@ -27,7 +27,7 @@ t_bool	is_dollar(char c)
 		return (FALSE);
 }
 
-t_bool has_heredoc(t_heredoc_to_fd *heredoc)
+t_bool	has_heredoc(t_heredoc_to_fd *heredoc)
 {
 	if (heredoc)
 		return (TRUE);

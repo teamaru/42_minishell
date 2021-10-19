72d8dfeb2227b3da45030b7936a6d29cc88afe89
public-jun <generalpublicjunjun@gmail.com>
2021-10-19 23:15:25 +0900

    Add: norm #36

--- srcs/request/request.c
+++ srcs/request/request.c
@@ -6,28 +6,28 @@
 /*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/08 14:45:50 by tsugiyam          #+#    #+#             */
-/*   Updated: 2021/10/12 14:44:33 by jnakahod         ###   ########.fr       */
+/*   Updated: 2021/10/19 23:14:48 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <mini_shell.h>
 
-extern t_request g_request;
+extern t_request	g_request;
 
-t_bool process_request(char *line)
+t_bool	process_request(char *line)
 {
-  tokenize(&g_request.tokens, line);
-  if (!is_valid_syntax())
-    return (TRUE);
-  parse();
-  if (!expand())
-    return (TRUE);
-  if (!request_convert_to_pipe_list())
-    return (TRUE);
-  return (TRUE);
+	tokenize(&g_request.tokens, line);
+	if (!is_valid_syntax())
+		return (TRUE);
+	parse();
+	if (!expand())
+		return (TRUE);
+	if (!request_convert_to_pipe_list())
+		return (TRUE);
+	return (TRUE);
 }
 
-t_bool	request_convert_to_pipe_list()
+t_bool	request_convert_to_pipe_list(void)
 {
 	t_pipe_list	*pipe_list;
 
@@ -36,17 +36,17 @@ t_bool	request_convert_to_pipe_list()
 		return (FALSE);
 	execute_cmds(pipe_list);
 	free_pipe_list(pipe_list);
-  return (TRUE);
+	return (TRUE);
 }
 
-void init_request()
+void	init_request(void)
 {
-  g_request.tokens = NULL;
-  g_request.cmds = NULL;
-  g_request.cmd = NULL;
-  g_request.builtin_id = NON_BUILTIN;
-  init_builtin_funcs();
-  g_request.option = NON;
-  g_request.arguments = NULL;
-  g_request.excution = FALSE;
+	g_request.tokens = NULL;
+	g_request.cmds = NULL;
+	g_request.cmd = NULL;
+	g_request.builtin_id = NON_BUILTIN;
+	init_builtin_funcs();
+	g_request.option = NON;
+	g_request.arguments = NULL;
+	g_request.excution = FALSE;
 }
